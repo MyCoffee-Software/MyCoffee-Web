f3c37e4699a19321f2a8ce673ddee61aad16f8d5
Yagosbk <yagosbk@hotmail.com>
2024-04-01 00:14:32 -0300

    Implementando aplicação base.
    Implementando a aplicação base Hello World.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './iconeCafe.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a>
+          Inicio projeto MyCoffee Web
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
