fd32747875a12250f34161e212f63bb91d93937f
Luan Rohwedder <luan.sandim@evop.com.br>
2024-04-19 20:54:01 -0400

    Redirecionamento do produto

--- src/components/Cards/ProductCard/index.js
+++ src/components/Cards/ProductCard/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import * as C from "./styles";
 import MediaQuery from 'react-responsive';
+import { Link } from 'react-router-dom';
 
 const ProductCard = ({ product, handleBuy }) => {
   return (
     <C.ProductWrapper>
-      <C.ImageContainer>
-        <C.Image src={product.image} alt={product.title} />
-      </C.ImageContainer>
+      <Link to={`/product/${product.id}`}>
+        <C.ImageContainer onClick={handleBuy}>
+          <C.Image src={product.image} alt={product.title} />
+        </C.ImageContainer>
+      </Link>
 
-      <C.line/>
+      <C.line />
 
       <C.NameContainer>
-        <C.Name>{product.title}</C.Name>
+        <C.StyledLink to={`/product/${product.id}`}>
+          <C.Name>{product.title}</C.Name>
+        </C.StyledLink>
       </C.NameContainer>
 
       <MediaQuery minWidth={1280}>
@@ -21,7 +26,7 @@ const ProductCard = ({ product, handleBuy }) => {
           <C.BuyButton onClick={handleBuy}>Comprar</C.BuyButton>
         </C.BottomWrapper>
       </MediaQuery>
-      
+
       <MediaQuery maxWidth={1279}>
         <C.Price>R${product.price}</C.Price>
         <C.BuyButton onClick={handleBuy}>Comprar</C.BuyButton>
