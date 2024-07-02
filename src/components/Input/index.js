2146da9d083fae413106c29be8269786b6c09939
Luan Rohwedder <luan-rohwedder@hotmail.com>
2024-07-01 20:37:06 -0400

    :hammer: Refactor: Implementação de endereços e mask para input de clientes

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,19 +1,45 @@
-import React from 'react'
-import * as C from "./styles"
+import React from 'react';
+import InputMask from 'react-input-mask';
+import * as C from "./styles";
 
-const Input = ({ type, placeholder, value, onChange, disabled, name, onKeyDown }) => {
-  return (
-    <C.Input
-      className='input'
-      defaultValue={value}
-      onChange={onChange}
-      type={type}
-      placeholder={placeholder}
-      disabled={disabled}
-      name={name}
-      onKeyDown={onKeyDown}
-    />
-  );
+const Input = ({ type, placeholder, value, onChange, disabled, name, onKeyDown, mask }) => {
+  const renderInput = () => {
+    if (mask) {
+      return (
+        <InputMask
+          mask={mask}
+          value={value}
+          onChange={onChange}
+          disabled={disabled}
+          onKeyDown={onKeyDown}
+        >
+          {() => (
+            <C.Input
+              className='input'
+              type={type}
+              placeholder={placeholder}
+              name={name}
+            />
+          )}
+        </InputMask>
+      );
+    } else {
+      return (
+        <C.Input
+          className='input'
+          defaultValue={value}
+          onChange={onChange}
+          type={type}
+          placeholder={placeholder}
+          disabled={disabled}
+          name={name}
+          onKeyDown={onKeyDown}
+        />
+      );
+    }
+  };
+
+  return renderInput();
 };
 
 export default Input;
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -5,6 +5,7 @@ import { faCog } from '@fortawesome/free-solid-svg-icons';
 import InfoInput from '../../components/InfoInput';
 import Button from '../../components/Button';
 import { toast, ToastContainer } from 'react-toastify';
+import { formatCPF, isValidCPF, isValidEmail, isValidMobilePhone } from '@brazilian-utils/brazilian-utils';
 
 const Profile = () => {
   const { user, fetchUser } = useAuth();
@@ -16,7 +17,11 @@ const Profile = () => {
     address: "",
     date: "",
     phone: "",
-    cpf: ""
+    cpf: "",
+    cep: "",
+    estado: "",
+    cidade: "",
+    num: "",
   });
   const [userImage, setUserImage] = useState(null);
 
@@ -70,6 +75,10 @@ const Profile = () => {
         date: formattedDate,
         phone: client.telefone,
         address: client.endereco,
+        cep: client.cep,
+        state: client.estado,
+        city: client.cidade,
+        num: client.numero 
       });
     }
   }, [user, client]);
@@ -80,19 +89,61 @@ const Profile = () => {
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
+    let newValue = value;
+    
+    if (name === 'cpf') {
+      const cpf = value.replace(/\D/g, '');
+      newValue = cpf;
+    }
+    
     setUserData((prevInputData) => ({
       ...prevInputData,
-      [name]: value,
+      [name]: newValue,
     }));
+
+    if (name === 'cep') {
+      const cepNumerico = value.replace(/\D/g, '');
+      if (cepNumerico.length === 8) {
+        handleCEP(cepNumerico);
+      }
+    }
   };
 
   const handlePasswordChange = () => {
 
   };
 
+  const handleCEP = async (cep) => {
+    try {
+      const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      if (!response.ok) {
+        throw new Error("Erro ao buscar o CEP");
+      }
+      const data = await response.json();
+
+      setUserData((prevInputData) => ({
+        ...prevInputData,
+        cep: cep,
+        address: data.logradouro,
+        city: data.localidade,
+        state: data.uf,
+      }))
+    } catch (error) {
+      toast.error('Erro ao buscar o CEP');
+      console.error("Error:", error);
+    }
+  };
+
   const handleSave = async () => {
     try {
-      console.log(userData);
+      if (!isValidEmail(userData.email)) {
+        throw new Error("Email inválido");
+      }
+  
+      if (!isValidMobilePhone(userData.phone)) {
+        throw new Error("Telefone inválido");
+      }
+
       const response = await fetch(`${process.env.REACT_APP_API_URL}/clientes?id=${user.id}`, {
         method: 'PUT',
         headers: {
@@ -103,6 +154,10 @@ const Profile = () => {
           cpf: userData.cpf,
           telefone: userData.phone,
           endereco: userData.address,
+          cep: userData.cep,
+          estado: userData.state,
+          cidade: userData.city,
+          numero: userData.num,
           usuario: {
             nomeCompleto: userData.name,
             email: userData.email,
@@ -115,13 +170,13 @@ const Profile = () => {
         throw new Error('Erro ao atualizar usuário');
       }
 
-      setEdit(false);
+      setEdit(!edit);
       fetchUser(localStorage.getItem("user_token"));
       toast.success('Usuário atualizado com sucesso!', {
         theme: "colored",
       });
     } catch (e) {
-      toast.error('Erro ao atualizar usuário');
+      toast.error(e.message);
       console.error("Error:", e);
     }
   }
@@ -145,17 +200,15 @@ const Profile = () => {
           <InfoInput title="Email" name="email" inputInfo={userData.email} disabled={edit} onChange={handleInputChange} />
           <InfoInput title="Data de nascimento" type="date" name="date" inputInfo={userData.date} disabled={edit} onChange={handleInputChange} />
           <InfoInput title="Telefone" name="phone" inputInfo={userData.phone} disabled={edit} onChange={handleInputChange} />
-          <InfoInput title="Endereço" name="adress" inputInfo={userData.address} disabled={edit} onChange={handleInputChange} />
+          <InfoInput title="CPF" name="cpf" inputInfo={userData.cpf} disabled={edit} onChange={handleInputChange} mask="999.999.999-99"/>
         </C.UserInfoColumn>
 
         <C.UserInfoColumn>
-          {/*
-          <InfoInput title="RG" inputInfo={userData.name} disabled={edit} onChange={handleInputChange} />
-          <InfoInput title="CEP" inputInfo={userData.name} disabled={edit} onChange={handleInputChange} />
-          <InfoInput title="Estado" inputInfo={userData.email} disabled={edit} onChange={handleInputChange} />
-          <InfoInput title="Cidade" inputInfo={userData.name} disabled={edit} onChange={handleInputChange} />
-          <InfoInput title="Número" inputInfo={userData} disabled={edit} onChange={handleInputChange} />
-          */}
+          <InfoInput title="Endereço" name="adress" inputInfo={userData.address} disabled={edit} onChange={handleInputChange} />
+          <InfoInput title="CEP" name="cep" inputInfo={userData.cep} disabled={edit} onChange={handleInputChange} mask="99999-999"/>
+          <InfoInput title="Estado" name="state" inputInfo={userData.state} disabled={edit} onChange={handleInputChange} />
+          <InfoInput title="Cidade" name="city" inputInfo={userData.city} disabled={edit} onChange={handleInputChange} />
+          <InfoInput title="Número" name="num" inputInfo={formatCPF(userData.num)} disabled={edit} onChange={handleInputChange} />
         </C.UserInfoColumn>
       </C.UserInfoWrapper>
 
