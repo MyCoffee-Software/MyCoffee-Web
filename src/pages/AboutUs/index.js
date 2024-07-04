8862d2f3c292036f9492e12512ae6b0a78380e85
Luan Rohwedder <luan-rohwedder@hotmail.com>
2024-07-04 02:32:34 -0400

    :hammer: Refactor: Mudando código para tela sobre nós

--- src/pages/AboutUs/index.js
+++ src/pages/AboutUs/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as C from './styles';
 import TextEditor from '../../components/TextEditor';
 import useAuth from '../../hooks/useAuth';
@@ -8,20 +8,52 @@ import { ToastContainer, toast } from 'react-toastify';
 import logo from "../../assets/logo_sobre_nos.svg";
 
 const AboutUs = () => {
-  const { user, permissions } = useAuth();
-  const [content, setContent] = useState('');
+  const { permissions } = useAuth();
+  const [content, setContent] = useState();
   const [edit, setEdit] = useState(false);
+
+  useEffect(() => {
+    const fetchContent = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/sobre`);
+        const data = await response.text();
+        console.log(data);
+        setContent(data);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+
+    fetchContent();
+  }, []);
 
   const toggleEdit = () => {
     setEdit(!edit);
   };
 
-  const handleSave = () => {
-    toggleEdit();
-    toast.success('Salvo!', {
-      theme: "colored",
+  const handleSave = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/sobre`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ content }),
       });
-  }
+      if (!response.ok) {
+        throw new Error('Erro ao salvar o conteúdo');
+      }
+      toggleEdit();
+      toast.success('Salvo!', {
+        theme: "colored",
+      });
+    } catch (e) {
+      toast.error('Erro ao salvar.', {
+        theme: "colored",
+      });
+      console.log(e.message);
+    }
+  };
 
   function sanitize(content) {
     return DOMPurify.sanitize(content);
@@ -29,20 +61,18 @@ const AboutUs = () => {
 
   return (
     <>
-     <C.LogoContainer>
-      <C.Logo src={logo} alt="Image" />
-      <C.LogoText>SOBRE NÓS</C.LogoText>
-      
-       
-    </C.LogoContainer>
+      <C.LogoContainer>
+        <C.Logo src={logo} alt="Image" />
+        <C.LogoText>SOBRE NÓS</C.LogoText>
+      </C.LogoContainer>
       <C.Container>
         <C.ContentContainer>
-          {permissions?.includes("Administrador") && (  
+          {permissions?.includes("Administrador") && (
             <C.IconContainer>
-              { edit ? (
-                <C.IconButton onClick={handleSave} icon={faCheck} size='xl'/>
+              {edit ? (
+                <C.IconButton onClick={handleSave} icon={faCheck} size='xl' />
               ) : (
-                <C.IconButton onClick={toggleEdit} icon={faCog} size='xl'/>
+                <C.IconButton onClick={toggleEdit} icon={faCog} size='xl' />
               )}
             </C.IconContainer>
           )}
@@ -53,7 +83,7 @@ const AboutUs = () => {
               <div dangerouslySetInnerHTML={{ __html: sanitize(content) }} />
             )}
           </C.TextContainer>
-        </C.ContentContainer>  
+        </C.ContentContainer>
       </C.Container>
       <ToastContainer />
     </>
--- src/pages/AboutUs/styles.js
+++ src/pages/AboutUs/styles.js
@@ -4,24 +4,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top:100px
-  margin-top:100px
-`;
-
-export const BannerContainer = styled.div`
-  max-width: 100%;
-  margin: 0 auto;
-  margin-top: -20px;
-  flex: 1;
-  margin-top:100px
-`;
-
-export const BannerImage = styled.img`
-  width: 100%; 
-  height: auto;
-  margin: 0 auto;
-  margin-bottom: 20px;
-  margin-top:100px
+  margin-top:100px;
 `;
 
 export const ContentContainer = styled.div`
@@ -30,7 +13,7 @@ export const ContentContainer = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
-   
+  margin-top:10px; 
 `;
 
 export const IconContainer = styled.div`
@@ -38,7 +21,7 @@ export const IconContainer = styled.div`
   justify-content: flex-end;
   margin-bottom: 10px;
   width: 95%;
-  margin-top:100px
+  margin-top:10px;
 `;
 
 export const IconButton = styled(FontAwesomeIcon)`
@@ -108,17 +91,17 @@ export const Logo = styled.img`
 export const LogoText = styled.div`
   position: absolute;
   margin-top: 25px;
-  left: 50%; /* Posiciona a esquerda do texto no centro horizontal */
-  transform: translate(-50%, -50%); /* Move o texto de volta metade da sua própria largura e altura, para centralizar completamente */
-  text-align: center; /* Alinha o texto ao centro */
-  color: white; /* Cor do texto */
-  font-family: 'Volkhov', serif; /* Fonte Volkhov */
-  font-size: 30px; /* Tamanho da fonte */
-  font-weight: bold; /* Peso da fonte */
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: white;
+  font-family: 'Volkhov', serif;
+  font-size: 30px;
+  font-weight: bold;
   text-shadow: 
     -1px -1px 0 #000,  
      1px -1px 0 #000,
     -1px  1px 0 #000,
-     1px  1px 0 #000; /* Sombra do texto para simular borda */
-  padding: 10px 20px; /* Espaçamento interno do texto */
+     1px  1px 0 #000;
+  padding: 10px 20px;
 `;
